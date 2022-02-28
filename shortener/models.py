ceb7e4055a92db5733616a874a37a9ebe5cd615f
episohe <maybe_be@naver.com>
2022-02-28 17:29:36 +0900

    :zap: Part7 chapter3-3

--- shortener/models.py
+++ shortener/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User as U
-
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
 class PayPlan(models.Model):
     name = models.CharField(max_length=20)
     price = models.IntegerField()
-    updated_at = models.DateField(auto_now=True)
-    create_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
 
 
 class Users(AbstractUser):
@@ -17,5 +18,5 @@ class Users(AbstractUser):
 
 
 # class UserDetail(models.Model):
-#     user = models.OneToOneField(U, on_delete=models.CASCADE)
+#     user = models.OneToOneField(Users, on_delete=models.CASCADE)
 #     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
